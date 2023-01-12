f446cdcab2d23e5ae37182e0d2e7b290492e8cc3
Plusb Preco <plusb21@gmail.com>
2023-01-12 18:57:12 +0900

    feat: set default enum entry

--- nxzr_core/src/controller/kind.rs
+++ nxzr_core/src/controller/kind.rs
@@ -1,14 +1,11 @@
 use strum::{Display, EnumString};
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord, Hash, Display, EnumString)]
+#[derive(
+    Clone, Copy, Default, Debug, Eq, PartialEq, PartialOrd, Ord, Hash, Display, EnumString,
+)]
 pub enum ControllerType {
     JoyConL,
     JoyConR,
+    #[default]
     ProController,
-}
-
-impl Default for ControllerType {
-    fn default() -> Self {
-        Self::ProController
-    }
 }
