587f4b70afdf8135a4a5d14bed1089072ce17052
Plusb Preco <plusb21@gmail.com>
2023-01-13 02:05:34 +0900

    feat: add ControllerInfo static constant

--- nxzr_core/src/controller/kind.rs
+++ nxzr_core/src/controller/kind.rs
@@ -1,3 +1,5 @@
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
 use strum::{Display, EnumString};
 
 #[derive(
@@ -9,3 +11,35 @@ pub enum ControllerType {
     #[default]
     ProController,
 }
+
+#[derive(Debug)]
+pub struct ControllerInfo {
+    pub id: u8,
+    pub connection_info: u8,
+}
+
+pub static CONTROLLER_INFO_MAP: Lazy<HashMap<ControllerType, ControllerInfo>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+    map.insert(
+        ControllerType::JoyConL,
+        ControllerInfo {
+            id: 0x01,
+            connection_info: 0x0E,
+        },
+    );
+    map.insert(
+        ControllerType::JoyConR,
+        ControllerInfo {
+            id: 0x02,
+            connection_info: 0x0E,
+        },
+    );
+    map.insert(
+        ControllerType::ProController,
+        ControllerInfo {
+            id: 0x03,
+            connection_info: 0x00,
+        },
+    );
+    map
+});
--- nxzr_core/src/lib.rs
+++ nxzr_core/src/lib.rs
@@ -6,11 +6,16 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use crate::controller::kind::CONTROLLER_INFO_MAP;
+
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let a = CONTROLLER_INFO_MAP.get(&controller::kind::ControllerType::ProController);
+        if let Some(aa) = a {
+            println!("{}", aa.id);
+            assert_eq!(aa.connection_info, 0x00);
+        }
     }
 }
