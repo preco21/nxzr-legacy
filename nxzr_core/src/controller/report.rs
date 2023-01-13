f635ef54378a8ef46385e05988adae5620068bed
Plusb Preco <plusb21@gmail.com>
2023-01-14 01:19:38 +0900

    fix: use ReportResult

--- nxzr_core/src/controller/report.rs
+++ nxzr_core/src/controller/report.rs
@@ -2,7 +2,7 @@ use strum::{Display, IntoStaticStr};
 
 #[derive(Clone, Copy, Debug, Display, Eq, PartialEq, Ord, PartialOrd, Hash, IntoStaticStr)]
 pub enum ReportError {
-    TooShort,
+    TooShortDataLength,
     Malformed,
     UnsupportedReportId,
     UnsupportedSubcommand,
@@ -27,38 +27,73 @@ impl InputReport {
         Self { data }
     }
 
-    pub fn with_bytes(data: impl AsRef<[u8]>) -> ReportResult<Self> {
-        let cloned_data = data.as_ref().to_vec();
-        if cloned_data.len() < 1 {
-            return Err(ReportError::TooShort);
-        }
-        if cloned_data[0] != 0xA1 {
-            return Err(ReportError::Malformed);
-        }
-        Ok(Self { data: cloned_data })
+    pub fn with_data(data: impl AsRef<[u8]>) -> ReportResult<Self> {
+        // 50 is a standard input report size in format: https://github.com/dekuNukem/Nintendo_Switch_Reverse_Engineering/blob/master/bluetooth_hid_notes.md#standard-input-report-format
+        Self::with_data_and_length(data, 50)
     }
 
-    pub fn clear_subcommand() {}
-    pub fn stick_data() {}
+    pub fn with_data_and_length(data: impl AsRef<[u8]>, length: usize) -> ReportResult<Self> {
+        let data_ref = data.as_ref();
+        let min_len = std::cmp::max(length, 50);
+        if data_ref.len() < min_len {
+            return Err(ReportError::TooShortDataLength);
+        }
+        if data_ref[0] != 0xA1 {
+            return Err(ReportError::Malformed);
+        }
+        Ok(Self {
+            data: data_ref.to_vec(),
+        })
+    }
+
+    pub fn clear_subcommand(&mut self) -> ReportResult<()> {
+        for i in 14..51 {
+            self.data[i] = 0x00;
+        }
+        Ok(())
+    }
+
+    pub fn stick_data(&self) -> &[u8] {
+        &self.data[7..13]
+    }
+
     pub fn subcommand_reply_data() {}
+
     pub fn input_report_id() {}
+
     pub fn set_input_report_id() {}
+
     pub fn set_timer() {}
+
     pub fn set_misc() {}
+
     pub fn set_button_status() {}
+
     pub fn set_stick_status() {}
+
     pub fn set_left_analog_stick() {}
+
     pub fn set_right_analog_stick() {}
+
     pub fn set_vibrator_input() {}
+
     pub fn ack() {}
+
     pub fn set_ack() {}
+
     pub fn set_6axis_data() {}
+
     pub fn set_ir_nfc_data() {}
+
     pub fn reply_to_subcommand_id() {}
+
     // FIXME:
     // pub fn get_reply_to_subcommand_id() {}
+
     pub fn sub_0x02_device_info() {}
+
     pub fn sub_0x10_spi_flash_read() {}
+
     pub fn sub_0x04_trigger_buttons_elapsed_time() {}
 
     pub fn as_bytes(&self) -> &[u8] {
@@ -179,7 +214,7 @@ impl OutputReport {
         let data_ref = data.as_ref();
         let min_len = std::cmp::max(length, 12);
         if data_ref.len() < min_len {
-            return Err(ReportError::TooShort);
+            return Err(ReportError::TooShortDataLength);
         }
         if data_ref[0] != 0xA2 {
             return Err(ReportError::Malformed);
@@ -231,11 +266,11 @@ impl OutputReport {
         }
     }
 
-    pub fn subcommand_data(&self) -> Option<&[u8]> {
+    pub fn subcommand_data(&self) -> ReportResult<&[u8]> {
         if self.data.len() < 13 {
-            return None;
+            return Err(ReportError::TooShortDataLength);
         }
-        Some(&self.data[12..])
+        Ok(&self.data[12..])
     }
 
     pub fn set_subcommand_data(&mut self, data: impl AsRef<[u8]>) -> ReportResult<()> {
@@ -249,8 +284,8 @@ impl OutputReport {
         if size > 0x1D || u32::from(size) + offset > 0x80000 {
             return Err(ReportError::OutOfRange);
         }
-        self.set_output_report_id(OutputReportId::SubCommand);
-        self.set_subcommand(Subcommand::SpiFlashRead);
+        self.set_output_report_id(OutputReportId::SubCommand)?;
+        self.set_subcommand(Subcommand::SpiFlashRead)?;
         for i in 12..12 + 4 {
             self.data[i] = u8::try_from(offset % 0x100).unwrap();
             offset = offset / 0x100;
