189b31d38f3223eced6d445becc143ac066775e6
Plusb Preco <plusb21@gmail.com>
2023-01-17 00:12:30 +0900

    chore: use lazy_static instead of once_cell

--- nxzr_core/src/controller/kind.rs
+++ nxzr_core/src/controller/kind.rs
@@ -1,4 +1,4 @@
-use once_cell::sync::Lazy;
+use lazy_static::lazy_static;
 use std::collections::HashMap;
 use strum::{Display, EnumString};
 
@@ -18,28 +18,30 @@ pub struct ControllerInfo {
     pub connection_info: u8,
 }
 
-pub static CONTROLLER_INFO_MAP: Lazy<HashMap<ControllerType, ControllerInfo>> = Lazy::new(|| {
-    let mut map = HashMap::new();
-    map.insert(
-        ControllerType::JoyConL,
-        ControllerInfo {
-            id: 0x01,
-            connection_info: 0x0E,
-        },
-    );
-    map.insert(
-        ControllerType::JoyConR,
-        ControllerInfo {
-            id: 0x02,
-            connection_info: 0x0E,
-        },
-    );
-    map.insert(
-        ControllerType::ProController,
-        ControllerInfo {
-            id: 0x03,
-            connection_info: 0x00,
-        },
-    );
-    map
-});
+lazy_static! {
+    pub static ref CONTROLLER_INFO_MAP: HashMap<ControllerType, ControllerInfo> = {
+        let mut map = HashMap::new();
+        map.insert(
+            ControllerType::JoyConL,
+            ControllerInfo {
+                id: 0x01,
+                connection_info: 0x0E,
+            },
+        );
+        map.insert(
+            ControllerType::JoyConR,
+            ControllerInfo {
+                id: 0x02,
+                connection_info: 0x0E,
+            },
+        );
+        map.insert(
+            ControllerType::ProController,
+            ControllerInfo {
+                id: 0x03,
+                connection_info: 0x00,
+            },
+        );
+        map
+    };
+}
