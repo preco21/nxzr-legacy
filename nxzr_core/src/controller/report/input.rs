09acd5cc312382fce5f05a79a18bdfccab6f643d
Plusb Preco <plusb21@gmail.com>
2023-01-19 01:34:08 +0900

    feat: implement sub_0x02_device_info

--- nxzr_core/src/controller/report/input.rs
+++ nxzr_core/src/controller/report/input.rs
@@ -1,4 +1,5 @@
-use super::{ReportError, ReportResult};
+use super::{subcommand::Subcommand, ReportError, ReportResult};
+use crate::controller::kind::ControllerInfo;
 use strum::Display;
 
 #[derive(Clone, Copy, Debug, Display, Eq, PartialEq, Ord, PartialOrd, Hash)]
@@ -59,16 +60,16 @@ impl InputReport {
     }
 
     pub fn with_data_and_len(data: impl AsRef<[u8]>, len: usize) -> ReportResult<Self> {
-        let data_ref = data.as_ref();
+        let data_r = data.as_ref();
         let min_len = std::cmp::max(len, 50);
-        if data_ref.len() < min_len {
+        if data_r.len() < min_len {
             return Err(ReportError::TooShort);
         }
-        if data_ref[0] != 0xA1 {
+        if data_r[0] != 0xA1 {
             return Err(ReportError::Malformed);
         }
         Ok(Self {
-            data: data_ref.to_vec(),
+            data: data_r.to_vec(),
         })
     }
 
@@ -158,21 +159,50 @@ impl InputReport {
         }
     }
 
+    // Returns `true` if the total data length matches 313
     pub fn set_ir_nfc_data(&mut self, data: impl AsRef<[u8]>) -> ReportResult<bool> {
-        let data_ref = data.as_ref();
-        if data_ref.len() > 313 {
-            return Err(ReportError::OutOfRange);
+        let data_r = data.as_ref();
+        if data_r.len() > 313 {
+            return Err(ReportError::OutOfBounds);
         }
         self.data
-            .splice(50..50 + data_ref.len(), data_ref.iter().cloned());
-        Ok(data_ref.len() == 313)
+            .splice(50..50 + data_r.len(), data_r.iter().cloned());
+        Ok(data_r.len() == 313)
     }
 
-    pub fn reply_to_subcommand_id(&self) {}
+    pub fn reply_to_subcommand_id(&self) -> Subcommand {
+        Subcommand::from_byte(self.data[15])
+    }
 
-    pub fn set_reply_to_subcommand_id(&self) {}
+    pub fn set_reply_to_subcommand_id(&mut self, id: Subcommand) -> ReportResult<()> {
+        match id.try_to_byte() {
+            Some(byte) => {
+                self.data[15] = byte;
+                Ok(())
+            }
+            None => Err(ReportError::UnsupportedSubcommand),
+        }
+    }
 
-    pub fn sub_0x02_device_info(&self) {}
+    pub fn sub_0x02_device_info(
+        &mut self,
+        mac_addr: [u8; 6],
+        fm_version: Option<[u8; 2]>,
+        controller_info: ControllerInfo,
+    ) -> ReportResult<()> {
+        let fm_version_u = match fm_version {
+            Some(version) => version,
+            None => [0x04, 0x00],
+        };
+        self.set_reply_to_subcommand_id(Subcommand::RequestDeviceInfo)?;
+        self.data.splice(16..16 + 2, fm_version_u);
+        self.data[16 + 2] = controller_info.id;
+        self.data[16 + 3] = 0x02;
+        self.data.splice(16 + 4..16 + 10, mac_addr);
+        self.data[16 + 10] = 0x01;
+        self.data[16 + 11] = 0x01;
+        Ok(())
+    }
 
     pub fn sub_0x10_spi_flash_read(&self) {}
 
