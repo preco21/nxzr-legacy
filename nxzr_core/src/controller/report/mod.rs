bf2d1e3ddd137af9f9897ecbe8ecb60816ee7b1b
Plusb Preco <plusb21@gmail.com>
2023-01-19 13:37:11 +0900

    docs: add some docs

--- nxzr_core/src/controller/report/mod.rs
+++ nxzr_core/src/controller/report/mod.rs
@@ -6,11 +6,22 @@ pub mod subcommand;
 
 #[derive(Clone, Copy, Debug, Display, Eq, PartialEq, Ord, PartialOrd, Hash, IntoStaticStr)]
 pub enum ReportError {
+    // Indicates that given data has not enough length. Usually used in constructors.
     TooShort,
+    // Indicates that given data is malformed thus cannot be processed. Usually used in constructors.
     Malformed,
+    // There's no such report id.
     UnsupportedReportId,
+    // There's no such subcommand.
     UnsupportedSubcommand,
-    OutOfRange,
+    // Returned when accessing/processing data that do not support given bounds.
+    OutOfBounds,
+    // There's no data for a value within a range. Usually used instead of
+    // `OutOfBounds` for a return value of getter methods where `OutOfBounds` is
+    // not appropriate. Since it's more descriptive to indicate that you are
+    // accessing no-existent data than just saying data out-of-bounds.
+    NoData,
+    // Returned if any invariant violation happens.
     Invariant,
 }
 
