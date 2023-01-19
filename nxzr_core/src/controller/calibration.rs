9143b51a229af67361326deaa756b80695105a29
Plusb Preco <plusb21@gmail.com>
2023-01-20 01:21:33 +0900

    feat: implement right stick calibration

--- nxzr_core/src/controller/calibration.rs
+++ nxzr_core/src/controller/calibration.rs
@@ -25,10 +25,42 @@ impl StickCalibration {
             v_max_below_center,
         }
     }
-}
 
-pub struct LeftStickCalibration {
-    stick_calibration: StickCalibration,
+    pub fn with_left_stick_bytes(bytes: impl AsRef<[u8]>) -> Self {
+        let bytes_padded = to_u16_bytes(bytes);
+        let h_max_above_center: u16 = (bytes_padded[1] << 8) & 0xF00 | bytes_padded[0];
+        let v_max_above_center = (bytes_padded[2] << 4) | (bytes_padded[1] >> 4);
+        let h_center = (bytes_padded[4] << 8) & 0xF00 | bytes_padded[3];
+        let v_center = (bytes_padded[5] << 4) | (bytes_padded[4] >> 4);
+        let h_max_below_center = (bytes_padded[7] << 8) & 0xF00 | bytes_padded[6];
+        let v_max_below_center = (bytes_padded[8] << 4) | (bytes_padded[7] >> 4);
+        StickCalibration::new(
+            h_center,
+            v_center,
+            h_max_above_center,
+            v_max_above_center,
+            h_max_below_center,
+            v_max_below_center,
+        )
+    }
+
+    pub fn with_right_stick_bytes(bytes: impl AsRef<[u8]>) -> Self {
+        let bytes_padded = to_u16_bytes(bytes);
+        let h_center: u16 = (bytes_padded[1] << 8) & 0xF00 | bytes_padded[0];
+        let v_center = (bytes_padded[2] << 4) | (bytes_padded[1] >> 4);
+        let h_max_below_center = (bytes_padded[4] << 8) & 0xF00 | bytes_padded[3];
+        let v_max_below_center = (bytes_padded[5] << 4) | (bytes_padded[4] >> 4);
+        let h_max_above_center = (bytes_padded[7] << 8) & 0xF00 | bytes_padded[6];
+        let v_max_above_center = (bytes_padded[8] << 4) | (bytes_padded[7] >> 4);
+        StickCalibration::new(
+            h_center,
+            v_center,
+            h_max_above_center,
+            v_max_above_center,
+            h_max_below_center,
+            v_max_below_center,
+        )
+    }
 }
 
 fn to_u16_bytes(bytes: impl AsRef<[u8]>) -> Vec<u16> {
@@ -39,33 +71,3 @@ fn to_u16_bytes(bytes: impl AsRef<[u8]>) -> Vec<u16> {
         .map(|e| u16::from(e))
         .collect::<Vec<_>>()
 }
-
-impl LeftStickCalibration {
-    pub fn from_bytes(bytes: impl AsRef<[u8]>) -> Self {
-        let bytes_padded = to_u16_bytes(bytes);
-        let h_max_above_center: u16 = (bytes_padded[1] << 8) & 0xF00 | bytes_padded[0];
-        let v_max_above_center = (bytes_padded[2] << 4) | (bytes_padded[1] >> 4);
-        let h_center = (bytes_padded[4] << 8) & 0xF00 | bytes_padded[3];
-        let v_center = (bytes_padded[5] << 4) | (bytes_padded[4] >> 4);
-        let h_max_below_center = (bytes_padded[7] << 8) & 0xF00 | bytes_padded[6];
-        let v_max_below_center = (bytes_padded[8] << 4) | (bytes_padded[7] >> 4);
-        LeftStickCalibration {
-            stick_calibration: StickCalibration::new(
-                h_center,
-                v_center,
-                h_max_above_center,
-                v_max_above_center,
-                h_max_below_center,
-                v_max_below_center,
-            ),
-        }
-    }
-}
-
-struct RightStickCalibration {
-    stick_calibration: StickCalibration,
-}
-
-// impl RightStickCalibration {
-//     fn from_bytes(_9bytes: &[u8]) -> Self {
-//         let h_center = (_9bytes[1] << 8) & 0xF00 | _9bytes[0] as i32
