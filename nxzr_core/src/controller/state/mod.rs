0f82d4f4474d57f4693292fbafd3d6dba8aa530d
Plusb Preco <plusb21@gmail.com>
2023-01-22 00:32:11 +0900

    feat: add StateError

--- nxzr_core/src/controller/state/mod.rs
+++ nxzr_core/src/controller/state/mod.rs
@@ -1,4 +1,21 @@
+use strum::{Display, IntoStaticStr};
+
 pub mod button;
 pub mod stick;
 
+#[derive(Clone, Copy, Debug, Display, Eq, PartialEq, Ord, PartialOrd, Hash, IntoStaticStr)]
+pub enum StateError {
+    // Invalid value range has been entered.
+    InvalidRange,
+    // There is no calibration data available.
+    NoCalibrationDataAvailable,
+    // Returned if any invariant violation happens.
+    Invariant,
+}
+
+impl std::error::Error for StateError {}
+
+pub type StateResult<T> = Result<T, StateError>;
+
+#[derive(Clone, Debug)]
 pub struct ControllerState;
