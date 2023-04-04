5c6d29c56f80d30f3ecf7dfbfe3fd03639cc3cb7
Plusb Preco <plusb21@gmail.com>
2023-04-04 22:06:20 +0900

    refactor: move error struct in core lib

--- nxzr_core/src/lib.rs
+++ nxzr_core/src/lib.rs
@@ -1,2 +1,51 @@
 pub mod controller;
-pub mod error;
+
+use strum::{Display, IntoStaticStr};
+
+#[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct Error {
+    pub kind: ErrorKind,
+    pub message: String,
+}
+
+#[derive(Clone, Copy, Debug, Display, Eq, PartialEq, Ord, PartialOrd, Hash, IntoStaticStr)]
+pub enum ErrorKind {
+    Internal(InternalErrorKind),
+}
+
+#[derive(Clone, Copy, Debug, Display, Eq, PartialEq, Ord, PartialOrd, Hash, IntoStaticStr)]
+pub enum InternalErrorKind {
+    Io(std::io::ErrorKind),
+}
+
+impl Error {
+    pub(crate) fn new(kind: ErrorKind) -> Self {
+        Self {
+            kind,
+            message: String::new(),
+        }
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        if self.message.is_empty() {
+            write!(f, "{}", &self.kind)
+        } else {
+            write!(f, "{}: {}", &self.kind, &self.message)
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Self {
+            kind: ErrorKind::Internal(InternalErrorKind::Io(err.kind())),
+            message: err.to_string(),
+        }
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
