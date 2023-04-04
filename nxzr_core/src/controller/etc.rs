c76943f77df6026855114e3f1f563aba22686e62
Plusb Preco <plusb21@gmail.com>
2023-04-05 02:15:07 +0900

    feat: use option type

--- nxzr_core/src/controller/etc.rs
+++ nxzr_core/src/controller/etc.rs
@@ -1,19 +1,23 @@
 pub struct SendDelay(Option<u8>);
 
 impl SendDelay {
-    fn to_byte(&self) -> f64 {
+    pub fn new(mode: Option<u8>) -> Self {
+        Self(mode)
+    }
+
+    pub fn to_byte(&self) -> Option<f64> {
         let Some(mode) = self.0 else {
             // Subcommands replies only
-            return f64::INFINITY;
+            return Some(f64::INFINITY);
         };
         match mode {
-            0x3F => 1.0,
-            0x21 => f64::INFINITY,
+            0x3F => Some(1.0),
+            0x21 => Some(f64::INFINITY),
             // FIXME: revisit later for freq adjustment on procon 1/120
-            0x30 => 1.0 / 60.0,
-            0x31 => 1.0 / 60.0,
-            // Unknown mode resorts to default, assuming it 1/15
-            _ => 1.0 / 15.0,
+            0x30 => Some(1.0 / 60.0),
+            0x31 => Some(1.0 / 60.0),
+            // Unknown mode resorts to default, assuming it as 1/15
+            _ => None,
         }
     }
 }
