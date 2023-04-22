6e6cbf5ec69d33bdbb0491daf2de6deb944c4e3f
Plusb Preco <plusb21@gmail.com>
2023-04-23 02:39:19 +0900

    feat: update hid path

--- nxzr_transport/src/device.rs
+++ nxzr_transport/src/device.rs
@@ -1,5 +1,5 @@
 const HID_UUID: &str = "00001124-0000-1000-8000-00805f9b34fb";
-const HID_PATH: &str = "/org/bluez/nxzr/dev";
+const HID_PATH: &str = "/org/bluez/nxzr/hid0";
 
 #[derive(Debug)]
 pub struct ControllerDevice;
