074b40f595201a99ae781be544b1724d6451b5a3
Plusb Preco <plusb21@gmail.com>
2023-05-01 23:51:53 +0900

    feat: add interop layer for bluer::Address

--- nxzr_device/src/sock/mod.rs
+++ nxzr_device/src/sock/mod.rs
@@ -132,6 +132,19 @@ impl From<Address> for MacAddr6 {
     }
 }
 
+// For `bluer` compatibility
+impl From<bluer::Address> for Address {
+    fn from(addr: bluer::Address) -> Self {
+        Self(addr.0.into())
+    }
+}
+
+impl From<Address> for bluer::Address {
+    fn from(addr: Address) -> Self {
+        addr.0.into()
+    }
+}
+
 /// Bluetooth device address type.
 #[derive(
     Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Display, EnumString, FromPrimitive,
