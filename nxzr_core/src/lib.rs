bdd411bf8c8ee613da7ba4ab358eee8e3fd45be2
Preco Plusb <plusb21@gmail.com>
2023-05-15 00:18:06 +0900

    fix: remove aggregation error modules

--- nxzr_core/src/lib.rs
+++ nxzr_core/src/lib.rs
@@ -1,25 +1,4 @@
-use controller::{protocol::ControllerProtocolError, report::ReportError, state::StateError};
-use event::EventError;
-use protocol::ProtocolError;
-use thiserror::Error;
-
 pub mod controller;
 pub mod device;
 pub mod event;
 pub mod protocol;
-
-#[derive(Clone, Error, Debug)]
-pub enum Error {
-    #[error(transparent)]
-    Report(#[from] ReportError),
-    #[error(transparent)]
-    State(#[from] StateError),
-    #[error(transparent)]
-    ControllerProtocol(#[from] ControllerProtocolError),
-    #[error(transparent)]
-    Protocol(#[from] ProtocolError),
-    #[error(transparent)]
-    Event(#[from] EventError),
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
--- nxzr_device/src/lib.rs
+++ nxzr_device/src/lib.rs
@@ -1,9 +1,3 @@
-use device::DeviceError;
-use helper::HelperError;
-use session::SessionError;
-use syscheck::SysCheckError;
-use transport::TransportError;
-
 pub mod device;
 pub mod helper;
 pub mod semaphore;
@@ -11,19 +5,3 @@ pub mod session;
 pub mod sock;
 pub mod syscheck;
 pub mod transport;
-
-#[derive(Clone, thiserror::Error, Debug)]
-pub enum Error {
-    #[error(transparent)]
-    Transport(#[from] TransportError),
-    #[error(transparent)]
-    Session(#[from] SessionError),
-    #[error(transparent)]
-    Device(#[from] DeviceError),
-    #[error(transparent)]
-    Helper(#[from] HelperError),
-    #[error(transparent)]
-    SysCheck(#[from] SysCheckError),
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
