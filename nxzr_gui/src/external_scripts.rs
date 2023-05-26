6fe593f8fef57f18d5b4259bd5442183cc282e5b
Plusb Preco <plusb21@gmail.com>
2023-05-27 02:29:40 +0900

    feat: add install_system_requirements

--- nxzr_gui/src/external_scripts.rs
+++ nxzr_gui/src/external_scripts.rs
@@ -1,27 +1,56 @@
-use std::io;
+use std::{
+    io::{self, Stderr, Stdout},
+    path::Path,
+    process::Stdio,
+};
 use tempfile::TempDir;
 use thiserror::Error;
-use tokio::{fs::File, io::AsyncWriteExt};
+use tokio::{fs::File, io::AsyncWriteExt, process::Command};
 
 const INSTALL_NXZR_SCRIPT: &str = include_str!("scripts/install-nxzr.ps1");
 
 #[derive(Error, Debug)]
 pub enum ExternalScriptError {
-    #[error("failed to install server: {0}")]
-    ServerInstallFailed(String),
+    #[error("failed to install NXZR: {0}")]
+    NxzrInstallFailed(String),
     #[error("failed to setup config: {0}")]
     ConfigSetupFailed(String),
     #[error("io: {0}")]
     Io(#[from] io::Error),
+    #[error("command failed: {0}")]
+    Command(#[from] SystemCommandError),
 }
 
+#[tracing::instrument(target = "gui_scripts")]
 pub async fn install_system_requirements() -> Result<(), ExternalScriptError> {
+    tracing::info!("installing system requirements");
     // Create a file from embedded install script to temp directory.
     let dir = TempDir::new()?;
-    println!("{:?}", dir);
-    let script_file_path = dir.path().join("install.ps1");
-    let mut file = File::create(script_file_path).await?;
-    file.write_all(INSTALL_NXZR_SCRIPT.as_bytes()).await?;
+    let script_path = dir.path().join("install.ps1");
+    let mut script_file = File::create(&script_path).await?;
+    script_file
+        .write_all(INSTALL_NXZR_SCRIPT.as_bytes())
+        .await?;
+    let Some(str_path) = script_path.to_str() else {
+        return Err(ExternalScriptError::NxzrInstallFailed("unable to convert script file path".to_owned()));
+    };
+    drop(script_file);
+    tracing::info!("created temporary install script at: {str_path}");
+    tracing::info!("running command...");
+    // FIXME: this will be closed immediately...
+    run_system_command({
+        let mut cmd = Command::new("powershell.exe");
+        cmd.args(&[
+            "-NoLogo",
+            "-NonInteractive",
+            "-WindowStyle",
+            "Normal",
+            "-File",
+            str_path,
+        ]);
+        cmd
+    })
+    .await?;
     Ok(())
 }
 
@@ -44,3 +73,23 @@ pub async fn install_system_requirements() -> Result<(), ExternalScriptError> {
 //     // sudo systemctl daemon-reload
 //     // sudo systemctl restart bluetooth
 // }
+
+#[derive(Error, Debug)]
+pub enum SystemCommandError {
+    #[error("failed to execute a command: {0}")]
+    CommandFailed(String),
+    #[error("utf8: {0}")]
+    Utf8Error(#[from] std::str::Utf8Error),
+    #[error("io: {0}")]
+    Io(#[from] io::Error),
+}
+
+async fn run_system_command(mut command: Command) -> Result<(), SystemCommandError> {
+    let output = command.output().await?;
+    if !output.status.success() {
+        return Err(SystemCommandError::CommandFailed(
+            std::str::from_utf8(&output.stderr)?.to_owned(),
+        ));
+    }
+    Ok(())
+}
--- nxzr_gui/src/main.rs
+++ nxzr_gui/src/main.rs
@@ -2,7 +2,12 @@ mod external_scripts;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    // Setup a tracer.
+    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+    // Check whether the program runs with elevated privileges.
     external_scripts::install_system_requirements().await?;
     Ok(())
 }
