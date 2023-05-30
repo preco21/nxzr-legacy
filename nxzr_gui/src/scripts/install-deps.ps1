6188c8e0987c769a6da78970f9cd0b98f773ad75
Plusb Preco <plusb21@gmail.com>
2023-05-31 02:23:39 +0900

    feat: create .wslconfig creator

--- nxzr_gui/src/scripts/install-deps.ps1
+++ nxzr_gui/src/scripts/install-deps.ps1
@@ -74,16 +74,20 @@ Write-Host "> This script will automatically install required dependencies of NX
 $tempdir = New-TemporaryDirectory
 Write-Host "> Using temporary directory: $tempdir"
 
+# Install WSL.
 Write-Host "> Installing the `"Windows Subsystem for Linux (WSL)`""
 Start-Process -FilePath "wsl.exe" -ArgumentList "--install --no-launch --web-download --no-distribution" -Wait -NoNewWindow
 # Start-Process -FilePath "winget.exe" -ArgumentList "install --source msstore --disable-interactivity --accept-source-agreements --accept-package-agreements `"Windows Subsystem for Linux`"" -Wait -NoNewWindow
 
+# Enable "Virtual Machine Platform" support.
 Write-Host "> Enabling `"Virtual Machine Platform`" component"
 Start-Process -FilePath "dism.exe" -ArgumentList "/online /enable-feature /featurename:VirtualMachinePlatform /all /norestart" -Wait -NoNewWindow
 
+# Check if there's any updates from WSL.
 Write-Host "> Checking for the `"Windows Subsystem for Linux (WSL)`" updates"
 Start-Process -FilePath "wsl.exe" -ArgumentList "--update --web-download" -Wait -NoNewWindow
 
+# Install `usbipd-win`.
 Write-Host "> Installing `"usbipd-win`""
 $usbipd_bin = Get-LatestGitHubReleaseBinary -Repo "dorssel/usbipd-win" -Dir $tempdir
 Install-Msi -FilePath $usbipd_bin
