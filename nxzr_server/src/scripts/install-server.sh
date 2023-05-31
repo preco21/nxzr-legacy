30af44ed350d16272a42a0b7d038be077732336f
Plusb Preco <plusb21@gmail.com>
2023-06-01 02:44:35 +0900

    chore: restart setup must come before actual commands

--- nxzr_server/src/scripts/install-server.sh
+++ nxzr_server/src/scripts/install-server.sh
@@ -4,11 +4,11 @@ set -e
 # This script is meant to be built-in to the binary directly, assuming that the
 # script is privileged already to perform required actions.
 
-# Ensure `apt` packages are all up-to-date.
-apt update && apt -y dist-upgrade
-
 # Mark `needrestart` config to work with a non-interactive TTY.
 sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+
+# Ensure `apt` packages are all up-to-date.
+apt update && apt -y dist-upgrade
 
 # Install required packages.
 #
