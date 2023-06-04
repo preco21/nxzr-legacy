4088fe31148c1b822e18d1e597cfc9a237af00c8
Preco Plusb <plusb21@gmail.com>
2023-06-05 04:26:01 +0900

    feat: add an example for checking paired devices

---
+++ nxzr_server/examples/check_paired_switches.rs
@@ -0,0 +1,18 @@
+use nxzr_device::{device, system};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Setup a tracer.
+    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+    system::prepare_device().await?;
+
+    let device = device::Device::new(device::DeviceConfig::default()).await?;
+    let paired_switches = device.paired_switches().await?;
+
+    tracing::info!("paired switches: {:?}", paired_switches);
+
+    Ok(())
+}
