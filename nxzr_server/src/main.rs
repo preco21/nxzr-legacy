928cb17c85af696c278f0ebe1722182e73d2ea8e
Preco Plusb <plusb21@gmail.com>
2023-06-06 04:45:37 +0900

    feat: add tracing relay

--- nxzr_server/src/main.rs
+++ nxzr_server/src/main.rs
@@ -1,18 +1,30 @@
 use nxzr_device::system;
 use server::ServerOpts;
-use tokio::signal;
+use tokio::{signal, sync::mpsc};
 use tracing_subscriber::prelude::*;
 
+mod common;
 mod controller;
 mod server;
-mod service;
+// mod service;
 // mod tracing;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let (log_tx, mut log_rx) = mpsc::unbounded_channel();
+    let writer_channel = common::WriterChannel::new(log_tx);
+
+    tokio::spawn(async move {
+        while let Some(t) = log_rx.recv().await {
+            println!("relay channel: {}", t);
+        }
+        println!("stream ended");
+    });
+
     // Setup a tracer.
     let subscriber = tracing_subscriber::registry()
         .with(tracing_subscriber::filter::LevelFilter::TRACE)
+        .with(tracing_subscriber::fmt::Layer::default().with_writer(writer_channel))
         .with(tracing_subscriber::fmt::Layer::default());
     tracing::subscriber::set_global_default(subscriber)?;
 
