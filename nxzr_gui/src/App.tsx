98360ddaf1f9b139ce6188f49bac4e953fd4d7c4
Preco Plusb <plusb21@gmail.com>
2023-06-14 02:06:17 +0900

    fix: fix lint

--- nxzr_gui/src/App.tsx
+++ nxzr_gui/src/App.tsx
@@ -1,29 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import React, { useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+import reactLogo from './assets/react.svg';
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+import './App.css';
+
+function App(): React.ReactElement {
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
+
+  const greet = async (): Promise<void> => {
+    setGreetMsg(await invoke('greet', { name }));
+  };
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
-        <a href="https://tauri.app" target="_blank">
+        <a href="https://tauri.app" target="_blank" rel="noreferrer">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -39,8 +40,8 @@ function App() {
       >
         <input
           id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
+          onChange={(e) => setName(e.currentTarget.value)}
         />
         <button type="submit">Greet</button>
       </form>
