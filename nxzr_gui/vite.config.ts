622197a3db6420a12348100396f97ec0010c9adc
Preco Plusb <plusb21@gmail.com>
2023-06-14 02:06:22 +0900

    chore: update vite config

--- nxzr_gui/vite.config.ts
+++ nxzr_gui/vite.config.ts
@@ -1,23 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
   clearScreen: false,
-  server: {
-    port: 1420,
-    strictPort: true,
-  },
-  // to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+  server: { port: 1420, strictPort: true },
+  envPrefix: ['VITE_', 'TAURI_'],
   build: {
-    // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
+    target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: Boolean(process.env.TAURI_DEBUG),
   },
 }));
