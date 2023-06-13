641d4cf41009fe2b07bb7788cc0695e0c985bc74
Preco Plusb <plusb21@gmail.com>
2023-06-14 02:24:27 +0900

    feat: add async example

--- nxzr_gui/src-tauri/src/main.rs
+++ nxzr_gui/src-tauri/src/main.rs
@@ -1,15 +1,75 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use tokio::sync::{mpsc, Mutex};
+
+struct AsyncProcInputTx {
+    inner: Mutex<mpsc::Sender<String>>,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+fn main() {
+    tracing_subscriber::fmt::init();
+
+    let (async_proc_input_tx, async_proc_input_rx) = mpsc::channel(1);
+    let (async_proc_output_tx, mut async_proc_output_rx) = mpsc::channel(1);
+
+    tauri::Builder::default()
+        .manage(AsyncProcInputTx {
+            inner: Mutex::new(async_proc_input_tx),
+        })
+        .invoke_handler(tauri::generate_handler![js2rs, greet])
+        .setup(|app| {
+            tauri::async_runtime::spawn(async move {
+                async_process_model(async_proc_input_rx, async_proc_output_tx).await
+            });
+
+            let app_handle = app.handle();
+            tauri::async_runtime::spawn(async move {
+                loop {
+                    if let Some(output) = async_proc_output_rx.recv().await {
+                        rs2js(output, &app_handle);
+                    }
+                }
+            });
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+fn rs2js<R: tauri::Runtime>(message: String, manager: &impl Manager<R>) {
+    tracing::info!(?message, "rs2js");
+    manager
+        .emit_all("rs2js", format!("rs: {}", message))
+        .unwrap();
+}
+
+#[tauri::command]
+async fn js2rs(message: String, state: tauri::State<'_, AsyncProcInputTx>) -> Result<(), String> {
+    tracing::info!(?message, "js2rs");
+    let async_proc_input_tx = state.inner.lock().await;
+    async_proc_input_tx
+        .send(message)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+async fn async_process_model(
+    mut input_rx: mpsc::Receiver<String>,
+    output_tx: mpsc::Sender<String>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    while let Some(input) = input_rx.recv().await {
+        let output = input;
+        output_tx.send(output).await?;
+    }
+
+    Ok(())
 }
