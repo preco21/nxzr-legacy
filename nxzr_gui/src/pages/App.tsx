7f4e7761bfa46ae97a22eeabd55528def8e9979f
Preco Plusb <plusb21@gmail.com>
2023-06-15 02:41:05 +0900

    feat: implement basic layout

--- nxzr_gui/src/pages/App.tsx
+++ nxzr_gui/src/pages/App.tsx
@@ -19,6 +19,21 @@ function App(): React.ReactElement {
     <MainContainer>
       <TitleBar />
       <Header />
+      <div
+        css={css`
+          display: grid;
+          grid-template-areas:
+            "content content sidebar"
+            "slider1 slider2 sidebar";
+          grid-template-rows: 130px 300px;
+          grid-template-columns: 1fr 1fr 1fr;
+        `}
+      >
+        <div css={css`grid-area: content; background-color: #009688`}>게임패드 미리보기</div>
+        <div css={css`grid-area: sidebar; background-color: blueviolet`}>키 바인딩 테이블</div>
+        <div css={css`grid-area: slider1; background-color: cornflowerblue`}>자이로 설정</div>
+        <div css={css`grid-area: slider2; background-color:maroon`}>스틱 설정</div>
+      </div>
     </MainContainer>
   );
 }
