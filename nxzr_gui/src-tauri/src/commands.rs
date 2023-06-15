c61d2b0411f59c2c262f516fa602173f2cfc806f
Preco Plusb <plusb21@gmail.com>
2023-06-16 00:57:16 +0900

    refactor: simplify async routines

---
+++ nxzr_gui/src-tauri/src/commands.rs
@@ -0,0 +1,4 @@
+#[tauri::command]
+pub async fn open_logs_window() {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
--- nxzr_gui/src-tauri/src/main.rs
+++ nxzr_gui/src-tauri/src/main.rs
@@ -3,17 +3,27 @@
 
 use tauri::Manager;
 use tokio::sync::{mpsc, Mutex};
+use tracing_subscriber::prelude::*;
+
+mod commands;
 
 struct AsyncProcInputTx {
     inner: Mutex<mpsc::Sender<String>>,
 }
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-fn main() {
-    tracing_subscriber::fmt::init();
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let module_filter =
+        tracing_subscriber::filter::Targets::new().with_target("nxzr_gui", tracing::Level::TRACE);
+    let subscriber = tracing_subscriber::registry().with(module_filter).with(
+        tracing_subscriber::fmt::Layer::default()
+            .event_format(tracing_subscriber::fmt::format().json()),
+    );
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    tauri::async_runtime::set(tokio::runtime::Handle::current());
 
     let (async_proc_input_tx, async_proc_input_rx) = mpsc::channel(1);
-    let (async_proc_output_tx, mut async_proc_output_rx) = mpsc::channel(1);
 
     tauri::Builder::default()
         .manage(AsyncProcInputTx {
@@ -21,18 +31,8 @@ fn main() {
         })
         .invoke_handler(tauri::generate_handler![js2rs, greet])
         .setup(|app| {
-            tauri::async_runtime::spawn(async move {
-                async_process_model(async_proc_input_rx, async_proc_output_tx).await
-            });
-
             let app_handle = app.handle();
-            tauri::async_runtime::spawn(async move {
-                loop {
-                    if let Some(output) = async_proc_output_rx.recv().await {
-                        rs2js(output, &app_handle);
-                    }
-                }
-            });
+            tokio::spawn(async move { async_process_model(async_proc_input_rx, app_handle).await });
 
             #[cfg(debug_assertions)]
             {
@@ -43,12 +43,25 @@ fn main() {
             Ok(())
         })
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .map_err(|err| anyhow::anyhow!("error while running application: {}", err))?;
+
+    Ok(())
 }
 
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+async fn async_process_model(
+    mut input_rx: mpsc::Receiver<String>,
+    app_handle: tauri::AppHandle,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    while let Some(input) = input_rx.recv().await {
+        let output = input;
+        rs2js(output, &app_handle);
+    }
+    Ok(())
 }
 
 fn rs2js<R: tauri::Runtime>(message: String, manager: &impl Manager<R>) {
@@ -66,16 +79,4 @@ async fn js2rs(message: String, state: tauri::State<'_, AsyncProcInputTx>) -> Re
         .send(message)
         .await
         .map_err(|e| e.to_string())
-}
-
-async fn async_process_model(
-    mut input_rx: mpsc::Receiver<String>,
-    output_tx: mpsc::Sender<String>,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    while let Some(input) = input_rx.recv().await {
-        let output = input;
-        output_tx.send(output).await?;
-    }
-
-    Ok(())
 }
