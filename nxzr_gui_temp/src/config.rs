7106e42ad34004ec59cc35ac3429d849a56b267d
Preco Plusb <plusb21@gmail.com>
2023-06-21 00:05:19 +0900

    feat: remove gui temp

--- nxzr_gui_temp/src/config.rs
+++
@@ -1,5 +0,0 @@
-pub const QUALIFIER: &str = "dev";
-pub const ORGANIZATION: &str = "Prevl";
-pub const APP_NAME: &str = "NXZR";
-
-pub const WSL_PASSWORD: &str = "7c56c0d2-b972-4098-94e3-1f96399657e2";
